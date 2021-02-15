a750fc21d19d1422d8b9159e40e4dfb6b4045c1e
John McLear <john@mclear.co.uk>
2021-02-15 13:51:45 +0000

    exit, need a better way but this will do for now :)

--- static/tests/backend/specs/test.js
+++ static/tests/backend/specs/test.js
@@ -4,11 +4,13 @@ const etherpad = require('../../../../index');
 
 describe('Client connectivity', function () {
   context('Connects', function () {
-    it('returns ok', function (done) {
+    it('Connected', function (done) {
+      this.timeout(10000);
       const pad = etherpad.connect('http://127.0.0.1:9001/p/test');
       pad.on('connected', (padState) => {
         console.log('Connected to', padState.host, 'with padId', padState.padId);
         done();
+        process.exit(0); /* eslint-disable-line no-process-exit */
       });
     });
   });
