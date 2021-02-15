c6fb73026ed4307395ae4c666f140fd34c8c7a81
John McLear <john@mclear.co.uk>
2021-02-15 13:32:11 +0000

    backend connectivity test

---
+++ static/tests/backend/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const etherpad = require('../../../index');
+
+describe('Client connectivity', function () {
+  context('Connects', function () {
+    it('returns ok', function (done) {
+      const pad = etherpad.connect('http://127.0.0.1:9001/p/test');
+      pad.on('connected', (padState) => {
+        console.log('Connected to', padState.host, 'with padId', padState.padId);
+        done();
+      });
+    });
+  });
+});
