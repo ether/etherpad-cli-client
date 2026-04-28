30935ef44a082ae92741c41cc28ec03ecdcbdfbd
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2026-04-28 20:42:24 +0200

    feat: use ts

--- static/tests/backend/specs/test.js
+++ static/tests/backend/specs/test.js
@@ -1,5 +1,5 @@
 import common from 'ep_etherpad-lite/tests/backend/common';
-import * as etherpad from '../../../../src/index.js';
+import * as etherpad from '../../../../dist/index.js';
 
 describe(__filename, function () {
   before(async function () {
@@ -9,7 +9,7 @@ describe(__filename, function () {
   it('connects', async function () {
     const pad = etherpad.connect(new URL('/p/test', common.baseUrl).href);
     const padState = await new Promise((resolve) => pad.on('connected', (s) => resolve(s)));
-    common.logger.info('Connected to', (padState as any).host, 'with padId', (padState as any).padId);
+    common.logger.info('Connected to', padState.host, 'with padId', padState.padId);
     pad.close();
   });
 });
